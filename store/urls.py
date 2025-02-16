af4b65801ebd7a1cdc582903a2281894840482d1
Abdul Qader Kagzi <abdulqaderkagzi@gmail.com>
2025-02-16 16:07:36 +0530

    Add Cart System and Checkout View

--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import signup_user, login_user, logout_user, home, game_detail
+from .views import signup_user, login_user, logout_user, home, game_detail, cart, add_to_cart, remove_from_cart, checkout
 
 urlpatterns = [
     path('', home, name='home'),
@@ -7,4 +7,8 @@ urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('game/<int:game_id>/', game_detail, name='game_detail'),
+    path('cart/', cart, name='cart'),
+    path('add_to_cart/<int:game_id>/', add_to_cart, name='add_to_cart'),
+    path('remove_from_cart/<int:game_id>/', remove_from_cart, name='remove_from_cart'),
+    path('checkout/', checkout, name='checkout'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Game
@@ -36,3 +37,28 @@ def home(request):
 def game_detail(request, game_id):
     game = get_object_or_404(Game, id=game_id)
     return render(request, 'store/game_detail.html', {'game': game})
+
+def add_to_cart(request, game_id):
+    cart = request.session.get('cart', {})  # Get cart from session
+    cart[str(game_id)] = cart.get(str(game_id), 0) + 1  # Increase quantity
+    request.session['cart'] = cart  # Save cart
+    messages.success(request, "Game added to cart!")
+    return redirect('cart')
+
+def remove_from_cart(request, game_id):
+    cart = request.session.get('cart', {})
+    if str(game_id) in cart:
+        del cart[str(game_id)]
+    request.session['cart'] = cart
+    messages.success(request, "Game removed from cart!")
+    return redirect('cart')
+
+def cart(request):
+    cart = request.session.get('cart', {})
+    games = Game.objects.filter(id__in=cart.keys())  # Get games in cart
+    cart_items = [{'game': game, 'quantity': cart[str(game.id)]} for game in games]
+    total_price = sum(item['game'].price * item['quantity'] for item in cart_items)
+    return render(request, 'store/cart.html', {'cart_items': cart_items, 'total_price': total_price})
+
+def checkout(request):
+    return render(request, 'store/checkout.html')
